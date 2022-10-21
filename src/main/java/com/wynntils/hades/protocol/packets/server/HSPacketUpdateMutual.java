9d8bbefa480af085bdd0bc5ec4c1ef37376e6653
kristofbolyai <49001742+kristofbolyai@users.noreply.github.com>
2022-10-21 17:29:16 +0200

    feat: HSPacketUpdateMutual packet contains more information

--- src/main/java/com/wynntils/hades/protocol/packets/server/HSPacketUpdateMutual.java
+++ src/main/java/com/wynntils/hades/protocol/packets/server/HSPacketUpdateMutual.java
@@ -10,16 +10,21 @@ import java.util.UUID;
  * Used for updating mutual friends/party users their status.
  */
 public class HSPacketUpdateMutual implements HadesPacket<IHadesClientAdapter> {
-
     UUID user;
-    double x, y, z;
-    double health, mana;
-    double maxHealth, maxMana;
+    String name;
+    float x, y, z;
+    int health, mana;
+    int maxHealth, maxMana;
+    String world;
+    boolean isMutualFriend;
+    boolean isMutualPartyMember;
+    boolean isMutualGuildMember;
 
     public HSPacketUpdateMutual() { }
 
-    public HSPacketUpdateMutual(UUID user, double x, double y, double z, double health, double maxHealth, double mana, double maxMana) {
+    public HSPacketUpdateMutual(UUID user, String name, float x, float y, float z, int health, int mana, int maxHealth, int maxMana, String world, boolean isMutualFriend, boolean isMutualPartyMember, boolean isMutualGuildMember) {
         this.user = user;
+        this.name = name;
         this.x = x;
         this.y = y;
         this.z = z;
@@ -27,62 +32,96 @@ public class HSPacketUpdateMutual implements HadesPacket<IHadesClientAdapter> {
         this.maxHealth = maxHealth;
         this.mana = mana;
         this.maxMana = maxMana;
+        this.world = world;
+        this.isMutualFriend = isMutualFriend;
+        this.isMutualPartyMember = isMutualPartyMember;
+        this.isMutualGuildMember = isMutualGuildMember;
     }
 
     public UUID getUser() {
         return user;
     }
 
-    public double getX() {
+    public String getName() {
+        return name;
+    }
+
+    public float getX() {
         return x;
     }
 
-    public double getY() {
+    public float getY() {
         return y;
     }
 
-    public double getZ() {
+    public float getZ() {
         return z;
     }
 
-    public double getHealth() {
+    public int getHealth() {
         return health;
     }
 
-    public double getMaxHealth() {
+    public int getMaxHealth() {
         return maxHealth;
     }
 
-    public double getMana() {
+    public int getMana() {
         return mana;
     }
 
-    public double getMaxMana() {
+    public int getMaxMana() {
         return maxMana;
+    }
+
+    public String getWorld() {
+        return world;
+    }
+
+    public boolean isMutualFriend() {
+        return isMutualFriend;
+    }
+
+    public boolean isMutualGuildMember() {
+        return isMutualGuildMember;
+    }
+
+    public boolean isMutualPartyMember() {
+        return isMutualPartyMember;
     }
 
     @Override
     public void readData(HadesBuffer buffer) {
         user = buffer.readUUID();
-        x = buffer.readDouble();
-        y = buffer.readDouble();
-        z = buffer.readDouble();
-        health = buffer.readDouble();
-        maxHealth = buffer.readDouble();
-        mana = buffer.readDouble();
-        maxMana = buffer.readDouble();
+        name = buffer.readString();
+        x = buffer.readFloat();
+        y = buffer.readFloat();
+        z = buffer.readFloat();
+        health = buffer.readInt();
+        maxHealth = buffer.readInt();
+        mana = buffer.readInt();
+        maxMana = buffer.readInt();
+        world = buffer.readString();
+        isMutualFriend = buffer.readBoolean();
+        isMutualPartyMember = buffer.readBoolean();
+        isMutualGuildMember = buffer.readBoolean();
     }
 
     @Override
     public void writeData(HadesBuffer buffer) {
         buffer.writeUUID(user);
-        buffer.writeDouble(x);
-        buffer.writeDouble(y);
-        buffer.writeDouble(z);
-        buffer.writeDouble(health);
-        buffer.writeDouble(maxHealth);
-        buffer.writeDouble(mana);
-        buffer.writeDouble(maxMana);
+        buffer.writeString(name);
+        buffer.writeFloat(x);
+        buffer.writeFloat(y);
+        buffer.writeFloat(z);
+        buffer.writeInt(health);
+        buffer.writeInt(maxHealth);
+        buffer.writeInt(mana);
+        buffer.writeInt(maxMana);
+        buffer.writeString(world);
+        buffer.writeBoolean(isMutualFriend);
+        buffer.writeBoolean(isMutualPartyMember);
+        buffer.writeBoolean(isMutualGuildMember);
     }
 
     @Override
